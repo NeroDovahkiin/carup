2e9b273dfefb6528db4cdf38b313848746bf8a6c
Nero <federixo_fgm@hotmail.com>
2019-08-20 00:56:59 -0300

    funcionalidad mapa

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-//$("#formsServicio").css("display","none");
+$("#formsServicio").css("display","none");
+$(".mapo").css("display","none");
+
+
 
 function Busqueda(){
+    event.preventDefault();
     $("#divDatosUsuario").css("display","none");
     $("#formsServicio").css("display","inherit");
 }
 
+function mostrarMapa(){
+    event.preventDefault();
+    $(".mapo").css("display","inherit");
+}
+
+function ocultarMapa(){
+    event.preventDefault();
+    $(".mapo").css("display","none");
+}
